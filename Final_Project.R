c45b7a228fae5fec8d77dbc44ab441a12493c0f1
Jaime Acosta González <j.acosta@alumnos.upm.es>
2019-12-20 21:09:06 +0100

    Predictions

--- Final_Project.R
+++ Final_Project.R
@@ -4,7 +4,7 @@ rm(list = ls())
 
 #Load DataSet
 Sys.setlocale("LC_ALL","English")
-Movies <- read.csv(file="./data/tmdb_5000_movies.csv" , header = TRUE , sep = ",")
+
 
 #Install and library packages
 #install.packages("lattice")
@@ -15,6 +15,10 @@ Movies <- read.csv(file="./data/tmdb_5000_movies.csv" , header = TRUE , sep = ",
 #install.packages("rpart")
 #install.packages("rpart.plot")
 #install.packages("ggvis")
+#install.packages('DMwR')
+#install.packages('arules')
+#install.packages('party')
+
 
 library(lattice)
 library(ggplot2)
@@ -24,9 +28,14 @@ library(rpart)
 library(stringr)
 library(rpart.plot)
 library(ggvis)
+library(grid)
+library(DMwR)
+library(Matrix)
+library(arules)
 
 # PREPROCESSING DATA
 
+Movies <- read.csv(file="./data/tmdb_5000_movies.csv" , header = TRUE , sep = ",")
 Movies$spoken_languages <- NULL
 Movies$title <- NULL
 Movies$homepage <- NULL
@@ -35,9 +44,14 @@ Movies$keywords <- NULL
 Movies$genres <- NULL
 Movies$production_countries <- NULL
 Movies$production_companies <- NULL
+Movies$id <- NULL
 
 Movies <- Movies %>%
   mutate(original_language = factor(original_language), )
+
+
+Movies <- Movies %>%
+  mutate(release_date = as.Date(release_date), )
 ##we wont consider either the movies with budget or revenue that equals 0, as we suppose that this is an error.
 Movies$name_length <- str_length(Movies$original_title)
 Movies$original_title <- NULL
@@ -64,8 +78,8 @@ trainIndex <- createDataPartition(
 )
 
 # Subset your data into training and testing set
-training_set <- movies[ trainIndex, ] # Use indices to get training data
-test_set <- movies[ -trainIndex, ] # Remove train indices to get test data
+training_set <- Movies[ trainIndex, ] # Use indices to get training data
+test_set <- Movies[ -trainIndex, ] # Remove train indices to get test data
 
 # Specify cross validation approach: 10 fold CV
 fitControl <- trainControl(
@@ -73,224 +87,51 @@ fitControl <- trainControl(
   number = 10 # 10 fold
 )
 
-
+cat("\014") 
 
 # -----------------------HASTA AQUÍ BIEN---------------------------------------
+#
 
-#Specify cross validation approach:10 fold CV
-fitControl <- trainControl(method = "cv",
-                           number = 10)
+#2. Does the duration of film influence the critic score?
+linearMod <- lm(vote_average ~ runtime, data=training_set)
+distPred <- predict(linearMod, test_set)
+
+actuals_preds <- data.frame(cbind(actuals=test_set$vote_average, predicteds=distPred))
+correlation_accuracy <- cor(actuals_preds)
+correlation_accuracy
+regr.eval(actuals_preds$actuals, actuals_preds$predicteds)
+
+
+#4. Have old films better critic scores than recent ones?
+linearMod <- lm( vote_average ~ release_date, data=training_set)
+distPred <- predict(linearMod, test_set)
+
+actuals_preds <- data.frame(cbind(actuals=test_set$vote_average, predicteds=distPred))
+correlation_accuracy <- cor(actuals_preds)
+correlation_accuracy
+regr.eval(actuals_preds$actuals, actuals_preds$predicteds)
+
+
+#1. What attributes makes the film better (with better we refer to the money earned, the views, the ratings)?
+#6. The release date influences in the popularity and revenue of a movie?
+
 #Create a grid of parameters to search
 grid <- expand.grid(k=1:20)
 
-##1.What attributes makes the film better (with better we refer to the money earned, the views, the ratings)?
-#1.1 about the earned money
-fit_cv_grid_revenue <- train(popularity~revenue,
-                        data = test_data,
-                        preProcess = "range",
-                        method = "knn",
-                        trControl = fitControl,
-                        tuneGrid = grid)
-fit_cv_grid_revenue
-plot(test_data$popularity,test_data$revenue,
-abline(lm(test_data$popularity~test_data$revenue),cex=1.3,
-       pch = 16,xlab = "popularity",ylab="revenue"))
-
-
-#1.2 about the views
-fit_cv_grid_views <- train(popularity~vote_count,
-                             data = test_data,
-                             preProcess = "range",
+fit_cv_grid_revenue <- train(popularity~.,
+                             data = training_set,
                              method = "knn",
                              trControl = fitControl,
                              tuneGrid = grid)
-fit_cv_grid_views
-plot(test_data$popularity,test_data$vote_count,
-     abline(lm(test_data$popularity~test_data$vote_count),cex=1.3,
-            pch = 16,xlab = "popularity",ylab="vote_count"))
 
-#1.3 about the ratings
-fit_cv_grid_ratings <- train(popularity~vote_average,
-                             data = test_data,
-                             preProcess = "range",
-                             method = "knn",
-                             trControl = fitControl,
-                             tuneGrid = grid)
-fit_cv_grid_ratings
-plot(test_data$popularity,test_data$vote_average,
-     abline(lm(test_data$popularity~test_data$vote_average),cex=1.3,
-            pch = 16,xlab = "popularity",ylab="vote_average"))
+# Make predictions on the test set
+preds_cv_grid <- predict(fit_cv_grid_revenue, test_set)
 
+# Assess performance via a confusion matrix
+confusionMatrix(test_set$popularity, preds_cv_grid, positive = "M")
 
-##2.Does the duration of film influence the critic score?
-fit_cv_grid_runtime <- train(vote_average~runtime,
-                             data = test_data,
-                             preProcess = "range",
-                             method = "knn",
-                             trControl = fitControl,
-                             tuneGrid = grid)
-plot(test_data$vote_average,test_data$runtime,
-     abline(lm(test_data$vote_average~test_data$runtime),cex=1.3,
-            pch = 16,xlab = "vote_average",ylab="runtime"))
-##3.Are some film genres more popular than others? Have it changed during years?
-releaseYear <- str_split(test_data$release_date,"-")
-dfReYear <- data.frame(releaseYear)
-dfReYear <- dfReYear[-2,]
-dfReYear <- dfReYear[-2,]
-names(dfReYear) <- c(1:300)
-dfReYear <- data.frame(t(dfReYear))
-names(dfReYear) <- c("Year")
-test_data <- cbind(dfReYear,test_data)
-fit_cv_grid_releasedate <- train(genres~Year,
-                             data = test_data,
-                             method = "knn",
-                             trControl = fitControl,
-                             tuneGrid = grid)
-plot(test_data$genres,test_data$Year,
-     abline(lm(test_data$genres~test_data$Year),cex=1.3,
-            pch = 16,xlab = "genres",ylab="Year"))
+# Show the average performance (across folds) for each value of `k`
+ggplot(data = preds_cv_grid$results) +
+  geom_line(mapping = aes(x = k, y = Accuracy))
 
-##4.Have old films better critic scores than recent ones?
-fit_cv_grid_TimeAndScore <- train(vote_average~Year,
-                                 data = test_data,
-                                 method = "knn",
-                                 trControl = fitControl,
-                                 tuneGrid = grid)
-plot(test_data$vote_average,test_data$Year,
-     abline(lm(test_data$vote_average~test_data$Year),cex=1.3,
-            pch = 16,xlab = "vote_average",ylab="Year"))
-
-##5.Can a film earn a lot of money if the genre is not really popular in that year?
-fit_cv_grid_EarnedAndPopularity <- train(revenue~popularity,
-                                 data = test_data,
-                                 preProcess = "range",
-                                 method = "knn",
-                                 trControl = fitControl,
-                                 tuneGrid = grid)
-plot(test_data$revenue,test_data$popularity,
-     abline(lm(test_data$revenue~test_data$popularity),cex=1.3,
-            pch = 16,xlab = "revenue",ylab="popularity"))
-
-##6.The release date influences in the popularity and revenue of a movie?
-##Release date and Popularity
-fit_cv_grid_ReleaseAndPopularity <- train(popularity~Year,
-                                         data = test_data,
-                                         method = "knn",
-                                         trControl = fitControl,
-                                         tuneGrid = grid)
-plot(test_data$popularity,test_data$Year,
-     abline(lm(test_data$popularity~test_data$Year),cex=1.3,
-            pch = 16,xlab = "popularity",ylab="Year"))
-
-##Release date and Revenue
-fit_cv_grid_ReleaseAndRevenue <- train(revenue~Year,
-                                          data = test_data,
-                                          method = "knn",
-                                          trControl = fitControl,
-                                          tuneGrid = grid)
-
-plot(test_data$revenue,test_data$Year,
-     abline(lm(test_data$revenue~test_data$Year),cex=1.3,
-            pch = 16,xlab = "revenue",ylab="Year"))
-
-##Employing the algorithm of interest
-##1.What attributes makes the film better (with better we refer to the money earned, the views, the ratings)?
-#1.1 about the earned money
-Revenue <- cbind(test_data$popularity,test_data$revenue)
-dfRevenue <- data.frame(Revenue)
-names(dfRevenue) <- c("popularity","revenue")
-RevenueFit <- rpart(revenue~.,data=dfRevenue,method = "class")
-summary(RevenueFit)
-popularityNewData <- data.frame(popularity = dfRevenue$popularity)
-Revenuepredicted <- predict(RevenueFit,newdata = popularityNewData)
-rpart.plot(RevenueFit,branch=1,shadow.col = "gray",box.col="green",border.col="blue",split.col="red",split.cex=1.2)
-
-
-#1.2 about the views
-Views <- cbind(test_data$popularity,test_data$vote_count)
-dfViews <- data.frame(Views)
-names(dfViews) <- c("popularity","Views")
-ViewsFit <- rpart(Views~.,data=dfViews,method = "class")
-summary(ViewsFit)
-popularityNewData <- data.frame(popularity = dfViews$popularity)
-Viewspredicted <- predict(ViewsFit,newdata = popularityNewData)
-rpart.plot(ViewsFit,branch=1,shadow.col = "gray",box.col="green",border.col="blue",split.col="red",split.cex=1.2)
-
-
-#1.3 about the ratings
-Ratings <- cbind(test_data$popularity,test_data$vote_average)
-dfRatings <- data.frame(Ratings)
-names(dfRatings) <- c("popularity","Ratings")
-RatingsFit <- rpart(Ratings~.,data=dfRatings,method = "class")
-summary(RatingsFit)
-popularityNewData <- data.frame(popularity = dfRatings$popularity)
-Ratingspredicted <- predict(RatingsFit,newdata = popularityNewData)
-rpart.plot(RatingsFit,branch=1,shadow.col = "gray",box.col="green",border.col="blue",split.col="red",split.cex=1.2)
-
-
-##2.Does the duration of film influence the critic score?
-DurantionAndScore <- cbind(test_data$vote_average,test_data$runtime)
-dfDurAndScor <- data.frame(DurantionAndScore)
-names(dfDurAndScor) <- c("vote_average","runtime")
-DurAndScorFit <- rpart(runtime~.,data = dfDurAndScor,method = "class")
-summary(DurAndScorFit)
-ScoreNewData <- data.frame(vote_average = dfDurAndScor$vote_average)
-DurAndScorPredicted <- predict(DurAndScorFit,newdata = ScoreNewData)
-rpart.plot(DurAndScorFit,branch=1,shadow.col = "gray",box.col="green",border.col="blue",split.col="red",split.cex=1.2)
-
-
-##3.Are some film genres more popular than others? Have it changed during years?
-PopularityAndTime <- cbind(test_data$genres,test_data$Year)
-dfPopuAndTime <- data.frame(PopularityAndTime)
-names(dfPopuAndTime) <- c("genres","Year")
-PopuAndTimeFit <- rpart(Year~.,data = dfPopuAndTime,method = "class")
-summary(PopuAndTimeFit)
-popularityNewData2 <- data.frame(genres = dfPopuAndTime$genres)
-PopuAndTimePredicted <- predict(PopuAndTimeFit,newdata = popularityNewData2)
-rpart.plot(PopuAndTimeFit,branch=1,shadow.col = "gray",box.col="green",border.col="blue",split.col="red",split.cex=1.2)
-
-
-##4.Have old films better critic scores than recent ones?
-RatingsAndYear <- cbind(test_data$vote_average,test_data$Year)
-dfRatAndYear <- data.frame(RatingsAndYear)
-names(dfRatAndYear) <- c("vote_average","Year")
-RatingAndYearFit <- rpart(Year~.,data = dfRatAndYear,method = "class")
-summary(RatingAndYearFit)
-RatingNewData <- data.frame(vote_average = dfRatAndYear$vote_average)
-RatingAndYearPredicted <- predict(RatingAndYearFit,newdata = RatingNewData)
-rpart.plot(RatingAndYearFit,branch=1,shadow.col = "gray",box.col="green",border.col="blue",split.col="red",split.cex=1.2)
-
-
-##5.Can a film earn a lot of money if the genre is not really popular in that year?
-RevenueAndPopu <- cbind(test_data$revenue,test_data$popularity)
-dfReveAndPopu <- data.frame(RevenueAndPopu)
-names(dfReveAndPopu) <- c("revenue","popularity")
-RevenueAndPopuFit <- rpart(popularity~.,data = dfReveAndPopu,method = "class")
-summary(RevenueAndPopuFit)
-RevenueNewData <- data.frame(revenue = dfReveAndPopu$revenue)
-RevenueAndPopuPredicted <- predict(RevenueAndPopuFit,newdata = RevenueNewData)
-rpart.plot(RevenueAndPopuFit,branch=1,shadow.col = "gray",box.col="green",border.col="blue",split.col="red",split.cex=1.2)
-
-
-##6.The release date influences in the popularity and revenue of a movie?
-##Release date and Popularity
-PopularityAndTime <- cbind(test_data$popularity,test_data$Year)
-dfPopuAndTime <- data.frame(PopularityAndTime)
-names(dfPopuAndTime) <- c("popularity","Year")
-PopuAndTimeFit <- rpart(Year~.,data = dfPopuAndTime,method = "class")
-summary(PopuAndTimeFit)
-popularityNewData2 <- data.frame(popularity = dfPopuAndTime$popularity)
-PopuAndTimePredicted <- predict(PopuAndTimeFit,newdata = popularityNewData2)
-rpart.plot(PopuAndTimeFit,branch=1,shadow.col = "gray",box.col="green",border.col="blue",split.col="red",split.cex=1.2)
-
-
-##Release date and Revenue
-RevenueAndTime <- cbind(test_data$revenue,test_data$Year)
-dfRevenueAndTime <- data.frame(RevenueAndTime)
-names(dfRevenueAndTime) <- c("revenue","Year")
-RevenueAndTimeFit <- rpart(Year~.,data = dfRevenueAndTime,method = "class")
-summary(RevenueAndTimeFit)
-RevenueNewData2 <- data.frame(revenue = dfRevenueAndTime$revenue)
-RevenueAndTimePredicted <- predict(RevenueAndTimeFit,newdata = RevenueNewData2)
-rpart.plot(RevenueAndTimeFit,branch=1,shadow.col = "gray",box.col="green",border.col="blue",split.col="red",split.cex=1.2)
 
